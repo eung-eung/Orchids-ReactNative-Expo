47709beb26a2aedacd865cd661c88805597f4bdb
eung-eung <boyvip5231@gmail.com>
2024-02-29 01:12:03 +0700

    delete all favorite list feature

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,13 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
-import { useContext } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useContext, useLayoutEffect } from "react";
 import { ORCHIDS } from "../data/dummy-data";
 import { AppContext, FavoriteContext } from "../store/favorite-context";
 import OrchidsList from "../component/OrchidsList/OrchidsList";
 
-export default function FavoriteScreen() {
-    const { ids } = useContext(FavoriteContext)
+export default function FavoriteScreen({ navigation }) {
+    const { ids, deleteListFavorites } = useContext(FavoriteContext)
     const favoriteOrchids = ORCHIDS.filter(orchid => ids.includes(orchid.id))
-    console.log('ids: ', ids);
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return <View style={styles.deleteOuterContainer}>
+                    <View style={styles.deleteInnerContainer}>
+                        <Button color='red' title="Delete All" onPress={deleteListFavorites} />
+                    </View>
+                </View>
+            },
+
+        })
+    }, [navigation])
 
     if (ids.length === 0) {
         return <View style={styles.rootContainer}>
@@ -15,7 +27,10 @@ export default function FavoriteScreen() {
         </View>
     }
     return (
+
+
         <OrchidsList items={favoriteOrchids} />
+
     )
 }
 
@@ -29,5 +44,11 @@ const styles = StyleSheet.create({
         fontSize: 19,
         fontWeight: "bold",
         color: 'white'
-    }
+    },
+    deleteOuterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+
+    },
+
 })
