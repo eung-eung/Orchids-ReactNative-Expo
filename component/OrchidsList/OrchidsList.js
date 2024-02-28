4a2137e180bbd20b729e163d6417ee440a2a8730
eung-eung <boyvip5231@gmail.com>
2024-02-29 01:11:19 +0700

    scroll to top when change categoy

--- component/OrchidsList/OrchidsList.js
+++ component/OrchidsList/OrchidsList.js
@@ -1,8 +1,15 @@
 import { FlatList, StyleSheet, View } from "react-native"
-
+import { useEffect, useRef } from 'react'
 import OrchidItem from "./OrchidItem"
 
-export default function OrchidsList({ items }) {
+export default function OrchidsList({ items, isScrollToTop }) {
+    const flatList = useRef()
+
+    if (isScrollToTop) {
+        flatList.current.scrollToOffset({ animated: false, offset: 0 })
+    }
+
+
     const renderOrchidItem = itemData => {
         const item = itemData.item
         const orchidItemProps = {
@@ -17,9 +24,11 @@ export default function OrchidsList({ items }) {
     return (
         <View style={styles.container}>
             <FlatList
+                ref={flatList}
                 data={items}
                 keyExtractor={item => item.id}
                 renderItem={renderOrchidItem}
+                scrollIndicatorInsets={{ right: 1 }}
             />
         </View>
     )
@@ -27,7 +36,7 @@ export default function OrchidsList({ items }) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        // flex: 1,
 
     }
 })
